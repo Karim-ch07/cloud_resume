966283393de72e90628154e8c201fc0e7773426b
Karim-ch07 <chihanikarim07@gmail.com>
2023-04-07 20:37:33 +0200

    added counter

---
+++ get_api.js
@@ -0,0 +1,14 @@
+const countUrl = 'https://dm7lnllf0i.execute-api.us-east-1.amazonaws.com/prod'
+const countElement = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch(countUrl)
+        .then(res => res.json())
+        .then(res => {
+        countElement.innerHTML = res.visits;
+    });
+}
+
+
