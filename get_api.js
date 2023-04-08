0384e30d7ce38af2fbd56cddaa3d70684ffb4b91
Karim-ch07 <chihanikarim07@gmail.com>
2023-04-08 13:35:53 +0200

    added counter

--- get_api.js
+++ get_api.js
@@ -1,13 +1,13 @@
 const countUrl = 'https://u38uaetxc0.execute-api.us-east-1.amazonaws.com/prod/visitorcounter'
 const countElement = document.getElementById('count');
-
+const counter = 0
 updateVisitCount();
 
 function updateVisitCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.visits;
+        countElement.innerHTML = res.body;
     });
 }
 
