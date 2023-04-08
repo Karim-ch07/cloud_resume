2ee9a22ff626e8e1e758bf357c2f47c6b0df1f38
Karim-ch07 <chihanikarim07@gmail.com>
2023-04-08 16:26:59 +0200

    added counter

--- get_api.js
+++ get_api.js
@@ -1,9 +1,20 @@
-const countUrl = 'https://wwatps6x63.execute-api.us-east-1.amazonaws.com/prod'
+const countUrl = 'https://xz2967glfe.execute-api.us-east-1.amazonaws.com/prod'
 const countElement = document.getElementById('count');
 
 
 function updateVisitCount() {
     fetch(countUrl)
+   // {
+        //method: 'GET',
+       // headers: {
+
+            //"Access-Control-Allow-Headers": "Content-Type",
+            //"Access-Control-Allow-Origin": "*",
+            //"Access-Control-Allow-Methods": "GET"
+                //}
+   // }
+
+
         .then(res => res.json())
         .then(res => {
         console.log(res)
